9d2482db33bde02fb1c32ed62bebadccdbe6c898
magiwanders <magi.wanders@gmail.com>
2020-05-28 01:25:45 +0200

    DiscussionTest parziale

---
+++ primaprovainitinere/test/DiscussionTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import src.primaprovainitinere.Comment;
+import src.primaprovainitinere.Discussion;
+
+public class DiscussionTest {
+
+  
+
+}
