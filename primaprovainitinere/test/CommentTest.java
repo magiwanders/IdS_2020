3b2a8704a939240196e448467aa76b06ba1699a1
magiwanders <magi.wanders@gmail.com>
2020-05-27 12:43:35 +0200

    complete CommentTest

--- primaprovainitinere/test/CommentTest.java
+++ primaprovainitinere/test/CommentTest.java
@@ -1,5 +1,5 @@
 // javac -cp .:test/junit-4.13.jar test/CommentTest.java
-// java -cp ..:junit-4.13.jar:hamcrest-core-1.3.jar org.junit.runner.JUnitCore CommentTest
+// java -cp .:test/junit-4.13.jar:test/hamcrest-core-1.3.jar org.junit.runner.JUnitCore test.CommentTest
 package test;
 
 import org.junit.*;
@@ -12,33 +12,37 @@ public class CommentTest {
   private String comment = "This is a test comment";
   private int vote = 7;
 
-  private Comment comment111;
+  private Comment comment0;
+  private Comment comment1;
 
   @Before
   public void setUp() {
-    comment111 = new Comment(author, vote, comment);
-    Comment comment011 = new Comment("", vote, comment);
-    Comment comment101 = new Comment(author, 0, comment);
+    comment0 = new Comment("", 0, "");
+    comment1 = new Comment(author, vote, comment);
   }
 
   @Test
   public void getAuthorTest() {
-    assertEquals("TestAuthor didn't work", "TestAuthor", comment111.getAuthor());
+    assertEquals("getAuthor didn't work", author, comment1.getAuthor());
+    assertEquals("getAuthor didn't work", "", comment0.getAuthor());
   }
-/*
+
   @Test
   public void getVoteTest() {
-
+    assertEquals("getVote didn't work", vote, comment1.getVote());
+    assertEquals("getVote didn't work", 0, comment0.getVote());
   }
 
   @Test
-  public void getCommentTest() {
-
+  public void getBodyTest() {
+    assertEquals("getBody didn't work", comment, comment1.getBody());
+    assertEquals("getBody didn't work", "", comment0.getBody());
   }
 
   @Test
   public void toStringTest() {
-
+    assertEquals("toString didn't work", "[" + author + "] " + comment, comment1.toString());
+    assertEquals("toString didn't work", "[]", comment0.toString());
   }
-  */
+
 }
